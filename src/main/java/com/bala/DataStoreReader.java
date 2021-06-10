0230a7c5405163ed4bda2092b2ce4d15856ce98a
admindm <>
2021-06-10 15:28:38 -0500

    {/src/main/java/com/bala/DataStoreReader.java}

--- src/main/java/com/bala/DataStoreReader.java
+++ src/main/java/com/bala/DataStoreReader.java
@@ -4,20 +4,41 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * DataStoreReader
- */
 public class DataStoreReader {
 
-    public static List<String> getMerchantCodes() {
-        System.out.println("Loading values from data store");
-        // In this example, I'm just returning a static list of String.
-        /*
-         * However, you can write a suitable code to connect to an external data store
-         * (data grid or database or properties file or any other external system etc.,)
-         * that contains the list of values
-         */
-        return new ArrayList<String>(Arrays.asList(new String[] { "P1", "P2" }));
+	public static List<String> getMerchantCodes() {
+		System.out.println("Loading values from database");
 
-    }
+		List<String> listaCodigos = new ArrayList<String>();
+		try (Connection connection = DriverManager.getConnection(
+				"jdbc:postgresql://192.168.122.40:5432/listas", "fgonzalezc",
+				"redhat01")) {
+
+			System.out.println("Java JDBC PostgreSQL Example");
+			Class.forName("org.postgresql.Driver");
+
+			System.out.println("Connected to PostgreSQL database!");
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement
+					.executeQuery("SELECT codigo, descripcion FROM public.codcardiovascular");
+			while (resultSet.next()) {
+				System.out.printf("%-30.30s  %-30.40s%n",
+						resultSet.getString("codigo"),
+						resultSet.getString("descripcion"));
+				listaCodigos.add(resultSet.getString("codigo"));
+			}
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("PostgreSQL JDBC driver not found.");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("Connection failure.");
+			e.printStackTrace();
+		}
+
+		return listaCodigos;
+	}
+
+	public DataStoreReader() {
+	}
 }
