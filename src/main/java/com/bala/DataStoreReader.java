9b454c889a76070a78d4592e5f7213ce827066dd
fgonzalezc <>
2021-06-22 22:04:11 -0500

    {/src/main/java/com/bala/DataStoreReader.java}

--- src/main/java/com/bala/DataStoreReader.java
+++ src/main/java/com/bala/DataStoreReader.java
@@ -21,7 +21,7 @@ public class DataStoreReader {
 						"wusumotorreglas", "UnWy22dBxkL9JYuu")) {
 
 			System.out.println("Java JDBC Oracle Example");
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Class.forName("oracle");
 
 			System.out.println("Connected to Oracle database!");
 			Statement statement = connection.createStatement();
